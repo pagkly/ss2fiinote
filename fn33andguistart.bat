1b4dc750004730d1f2415c33ad55b3585035257e
pagkly <ajsgisayid@gmail.com>
2018-07-10 13:46:40 +1000

    commit by w10

--- fn33andguistart.bat
+++ fn33andguistart.bat
@@ -1,1 +1,1 @@
-%USERPROFILE%\Documents\Docs\Automate\3WinPython-32bit-3.5.3.1Qt5\scripts\python "%USERPROFILE%\Documents\GitHub\FN35OCVbside\FN33and.py"
+%USERPROFILE%\Documents\Docs\Automate\3WinPython-32bit-3.5.3.1Qt5\scripts\python.bat "%USERPROFILE%\Documents\GitHub\FN35OCVbside\FN33and.py"
