89bf3ba0b61b4cc2cdc112f63f047960db2203c4
pagkly <ajsgisayid@gmail.com>
2018-07-10 18:11:26 +1000

    commit by w10g

--- fn33andguistart.bat
+++ fn33andguistart.bat
@@ -1,1 +1,2 @@
+start "" "%USERPROFILE%\Documents\Docs\Automate\FiiNote\FiiNote.exe"
 %USERPROFILE%\Documents\Docs\Automate\3WinPython-32bit-3.5.3.1Qt5\scripts\python.bat "%USERPROFILE%\Documents\GitHub\FN35OCVbside\FN33and.py"
